483046d8f9c565f8fc594ba0d0dd1d72350487be
정이든 <2donny@naver.com>
2021-09-16 19:58:42 +0900

    Auth: reducer, state added

--- src/pages/auth/types.d.ts
+++ src/pages/auth/types.d.ts
@@ -1,12 +1,17 @@
 export interface AuthState {
+  uid: number;
+  email: string;
   phoneNumber: string;
   age: string;
   gender: string;
   name: string;
   university: string;
+  isGraduate: boolean;
   title: string;
   bio: string;
   location?: string;
+  profileImgUrl?: string;
+  profileImgFile?: File;
   agree1: boolean;
   agree2: boolean;
   agree3: boolean;
@@ -15,16 +20,20 @@ export interface AuthState {
   stage1Valid: boolean;
   stage2Valid: boolean;
 }
-
 export type AuthAction =
+  | { type: "setUid"; payload: number }
+  | { type: "setEmail"; payload: string }
   | { type: "setPhoneNumber"; payload: string }
   | { type: "setAge"; payload: string }
   | { type: "setGender"; payload: string }
   | { type: "setName"; payload: string }
   | { type: "setUniversity"; payload: string }
+  | { type: "setIsGraduate"; payload: boolean }
   | { type: "setTitle"; payload: string }
   | { type: "setBio"; payload: string }
   | { type: "setLocation"; payload: string }
+  | { type: "setProfileImgUrl"; payload: string }
+  | { type: "setProfileImgFile"; payload: File }
   | { type: "setAgree1"; payload: boolean }
   | { type: "setAgree2"; payload: boolean }
   | { type: "setAgree3"; payload: boolean }
