caebed86c685e97ea2e5ba9c496cbd0e7c9a63f9
정이든 <2donny@naver.com>
2021-09-17 15:45:51 +0900

    all constnats

---
+++ src/components/shared/constants.ts
@@ -0,0 +1,21 @@
+import { Option } from "react-dropdown";
+
+export const CURRENT_USER = "CURRENT_USER";
+
+export const placeLocationoptions: Option[] = [
+  {
+    value: "전체",
+    label: "전체",
+    className: "drop-down-option",
+  },
+  {
+    value: "안암",
+    label: "안암근처",
+    className: "drop-down-option",
+  },
+  {
+    value: "신촌",
+    label: "신촌근처",
+    className: "drop-down-option",
+  },
+];
