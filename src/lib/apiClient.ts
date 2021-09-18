693aa47467bd2bf06f31e793879823504d39a5bf
정이든 <2donny@naver.com>
2021-09-18 16:46:47 +0900

    env에 따른 host

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,15 +1,16 @@
 import { CURRENT_USER } from "./../components/shared/constants";
 import axios from "axios";
+import storage from "./storage";
 
-const data = window.localStorage.getItem(CURRENT_USER);
-const tk = data && JSON.parse(data).token;
-console.log(tk);
+const host =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/"
+    : process.env.REACT_APP_API_HOST || "/";
 
 const apiClient = axios.create({
-  baseURL: "http://172.30.1.27:5000/",
-  timeout: 8000,
+  baseURL: host,
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1OTEyMTllLTk1YmMtNDY0NS1hZDM2LTYxYzc3NzFhZWMyZSIsImlhdCI6MTYzMTg2Nzk3MSwiZXhwIjoxNjMxODc4NzcxfQ.fbPXKqWzSXHPKED3hOCsZpY27C79oe-Bwv83zixBy7w`,
+    Authorization: `Bearer ${storage.getItem(CURRENT_USER)?.token}`,
   },
 });
 
