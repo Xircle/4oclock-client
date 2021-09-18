303b97cb5ab956af22eadb66636bc6da928f3639
정이든 <2donny@naver.com>
2021-09-19 06:20:59 +0900

    Error handling

--- src/lib/api/seeRandomProfile.ts
+++ src/lib/api/seeRandomProfile.ts
@@ -1,10 +1,19 @@
 import { RandomProfileData, SeeRandomProfile } from "./types.d";
 import AxiosClient from "../apiClient";
 
-export const seeRandomProfile = async (): Promise<RandomProfileData> => {
-  const { data } = await AxiosClient.get<SeeRandomProfile>("user/friend");
-  if (!data.ok) {
-    alert(data.error);
+export const seeRandomProfile = async (): Promise<
+  RandomProfileData | undefined
+> => {
+  try {
+    const { data } = await AxiosClient.get<SeeRandomProfile>("/user/friend", {
+      timeout: 6000,
+    });
+    if (!data.ok) {
+      throw new Error(data.error);
+    }
+    return data.randomProfile;
+  } catch (err) {
+    console.log(err);
+    throw new Error("일시적인 에러가 발생했습니다.");
   }
-  return data.randomProfile;
 };
