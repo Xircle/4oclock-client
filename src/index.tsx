bf4ffecb2c4d55dd3f5377e8148a1a5f98145df6
정이든 <2donny@naver.com>
2021-09-19 06:21:09 +0900

    React-query QueryCache

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      toast.error("에러가 발생했습니다. 잠시후 이용해주세요.", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    },
+  }),
+});
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ToastContainer />
     <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
   document.getElementById("root")
