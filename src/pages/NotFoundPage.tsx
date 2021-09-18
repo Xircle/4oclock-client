fc02863099d720aa1f10ce0f52075a07fe50f44b
정이든 <2donny@naver.com>
2021-09-18 22:12:35 +0900

    Not found page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,23 @@
+import { useHistory } from "react-router-dom";
+import routes from "../routes";
+import { Heading } from "../styles";
+
+export default function NotFoundPage() {
+  const history = useHistory();
+  return (
+    <div
+      style={{
+        height: "80vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Heading>Not found</Heading>
+      <button onClick={() => history.push(routes.placeFeed)}>
+        홈으로 가기
+      </button>
+    </div>
+  );
+}
