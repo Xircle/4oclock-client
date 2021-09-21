13b203355600d3f42e820caa428cf06e4c54120c
정이든 <2donny@naver.com>
2021-09-22 07:15:02 +0900

    Add direct header

--- src/lib/api/getPlaceById.ts
+++ src/lib/api/getPlaceById.ts
@@ -1,9 +1,16 @@
 import { GetPlaceByIdOutput, PlaceData } from "./types.d";
 import AxiosClient from "../apiClient";
+import storage from "../storage";
+import { CURRENT_USER } from "../../components/shared/constants";
 
 export const getPlaceById = async (placeId: string): Promise<PlaceData> => {
   const { data } = await AxiosClient.get<GetPlaceByIdOutput>(
-    `/place/${placeId}`
+    `/place/${placeId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${storage.getItem(CURRENT_USER)["token"]}`,
+      },
+    }
   );
   if (!data.ok) {
     alert(data.error);
