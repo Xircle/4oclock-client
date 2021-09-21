37bf5c0d695cde876ed8c992f4119b3036060f68
정이든 <2donny@naver.com>
2021-09-21 19:04:11 +0900

    Add: add params

--- src/pages/my/MyPlacePage.tsx
+++ src/pages/my/MyPlacePage.tsx
@@ -6,16 +6,17 @@ import {
   Label,
 } from "../../styles";
 import BottomNavBar from "../../components/shared/BottomNavBar";
-import { Link } from "react-router-dom";
-import routes from "../../routes";
 import RegisteredFeed from "../../components/my/RegisteredFeed";
 import { Fragment, useEffect } from "react";
 import { useQuery } from "react-query";
 import { MyPlaceData } from "../../lib/api/types";
 import { getMyPlaces } from "../../lib/api/getMyPlaces";
 import BackButtonLayout from "../../components/shared/BackButtonLayout";
+import PageTitle from "../../components/PageTitle";
+import { useHistory } from "react-router-dom";
 
 export default function MyPlacePage() {
+  const history = useHistory();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -37,27 +38,23 @@ export default function MyPlacePage() {
 
   return (
     <ContainerFlexColumn>
+      <PageTitle title="My Xircle" />
       <BackButtonLayout>
         <ContainerwithLeftRightMargin>
           <Heading>내가 신청한 써클</Heading>
           {myPlacesData.length === 0 && <Label>신청한 써클이 없어요 :(</Label>}
           {myPlacesData.map((item) => {
             return (
-              <SLink
+              <RegisteredFeed
                 key={item.id}
-                to={{
-                  pathname: `/place/${item.id}`,
-                }}
-              >
-                <RegisteredFeed
-                  isClosed={item.isClosed}
-                  coverImage={item.coverImage}
-                  name={item.name}
-                  tags={"#" + JSON.parse(item.tags).join(" #")}
-                  startDateFromNow={item.startDateFromNow}
-                  recommendation={item.recommendation}
-                />
-              </SLink>
+                isClosed={item.isClosed}
+                coverImage={item.coverImage}
+                name={item.name}
+                tags={"#" + JSON.parse(item.tags).join(" #")}
+                startDateFromNow={item.startDateFromNow}
+                recommendation={item.recommendation}
+                onClick={() => history.push(`/place/${item.id}?myPlace=true`)}
+              />
             );
           })}
         </ContainerwithLeftRightMargin>
