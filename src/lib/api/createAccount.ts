f98fa337f7e2f963e30a0c3a7b1eb3d47802796c
정이든 <2donny@naver.com>
2021-09-23 18:00:24 +0900

    API: create account

--- src/lib/api/createAccount.ts
+++ src/lib/api/createAccount.ts
@@ -1,12 +1,29 @@
-import { CreateAccountInput, CreateAccountOutput } from "./types.d";
-import { AxiosResponse } from "axios";
+import { CreateAccountOutput } from "./types.d";
 import AxiosClient from "../apiClient";
+import { AuthState } from "../../components/auth/types";
 
 export const createAccount = async (
-  createAccountInput: CreateAccountInput
-): Promise<AxiosResponse<CreateAccountOutput>> => {
-  return AxiosClient.post<CreateAccountOutput>(
-    "/auth/social/register/kakao",
-    createAccountInput
+  state: AuthState
+): Promise<CreateAccountOutput> => {
+  const formData = new FormData();
+  state.profileImgFile
+    ? formData.append("profileImageFile", state.profileImgFile!)
+    : formData.append("profileImageUrl", state.profileImgUrl!);
+  formData.append("socialId", state.uid + "");
+  formData.append("email", state.email);
+  formData.append("phoneNumber", state.phoneNumber);
+  formData.append("username", state.name);
+  formData.append("university", state.university);
+  formData.append("isGraduate", state.isGraduate + "");
+  formData.append("age", state.age);
+  formData.append("gender", state.gender === "male" ? "Male" : "Female");
+  formData.append("job", state.title);
+  formData.append("shortBio", state.bio);
+  formData.append("location", state.location + "");
+  formData.append("isMarketingAgree", state.agree4 + "");
+  const { data } = await AxiosClient.post<CreateAccountOutput>(
+    "auth/social/register/kakao",
+    formData
   );
+  return data;
 };
