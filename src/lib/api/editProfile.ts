9d180f37e62b16a09cdf05f5bed92e22fc964102
정이든 <2donny@naver.com>
2021-09-24 19:16:15 +0900

    API: edit profile 연동 완료 :)

---
+++ src/lib/api/editProfile.ts
@@ -0,0 +1,20 @@
+import { CreateAccountOutput } from "./types";
+import AxiosClient from "../apiClient";
+import { CoreOutput } from "../../components/shared/types";
+import { ProfileData } from "../../pages/my/EditProfilePage";
+import { AxiosResponse } from "axios";
+
+export const editProfile = async (
+  editedProfileData: ProfileData
+): Promise<AxiosResponse<CoreOutput>> => {
+  const formData = new FormData();
+  editedProfileData.profileImageFile &&
+    formData.append("profileImagefile", editedProfileData.profileImageFile);
+  editedProfileData.username &&
+    formData.append("username", editedProfileData.username);
+  editedProfileData.shortBio &&
+    formData.append("shortBio", editedProfileData.shortBio);
+  editedProfileData.job && formData.append("job", editedProfileData.job);
+
+  return AxiosClient.put<CreateAccountOutput>("user", formData);
+};
--- src/pages/my/EditProfilePage.tsx
+++ src/pages/my/EditProfilePage.tsx
@@ -15,102 +15,130 @@ import {
   BigTextArea,
   Label,
   FileLabel,
-} from "../../styles";
-import { DummyAvartar, DummyProfileData } from "../../static/dummyData";
+} from "../../styles/styles";
+import { DummyProfileData } from "../../static/dummyData";
 import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BackButtonLayout from "../../components/shared/BackButtonLayout";
 import PageTitle from "../../components/PageTitle";
 import { useState, useEffect } from "react";
 import { getUser } from "../../lib/api/getUser";
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
+import ClipLoader from "react-spinners/ClipLoader";
 import { UserData } from "../../lib/api/types";
 import { AgeNumberToString } from "../../lib/utils";
+import { LoaderBackdrop, LoaderWrapper } from "../../components/shared/Loader";
+import diff from "object-diff";
+import _ from "lodash";
+import { editProfile } from "../../lib/api/editProfile";
+import { toast } from "react-toastify";
 import routes from "../../routes";
+import { RouteComponentProps } from "react-router-dom";
 
-interface Props {}
+export interface ProfileData {
+  username?: string;
+  shortBio?: string;
+  profileImageFile?: File;
+  profileImageUrl?: string;
+  job?: string;
+}
 
-export default function EditProfilePage(props: Props) {
-  const { data: userData, isLoading, refetch } = useQuery<UserData | undefined>(
-    "userProfile",
-    () => getUser(),
-    {
-      retry: 1,
-      refetchOnWindowFocus: false,
-    }
-  );
+interface Props extends RouteComponentProps {}
+
+export default function EditProfilePage({ history }: Props) {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
-  const [localImageSrc, setLocalImageSrc] = useState<string | undefined | null>(
-    userData?.profileImageUrl
-  );
-  // state.profileImgUrl
-  // set value here
-  const [localName, setLocalName] = useState<string | undefined | null>(
-    userData?.username
-    // set value here
-  );
-  const [localBio, setLocalBio] = useState<string | undefined | null>(
-    userData?.shortBio
-  );
-  const [localJob, setLocalJob] = useState<string | undefined | null>(
-    userData?.job
-  );
+  const [localProfileData, setLocalProfileData] = useState<ProfileData>({});
   const [localValidation, setLocalValidation] = useState<boolean[]>([
     true,
     true,
     true,
   ]);
 
-  useEffect(() => {
-    setLocalImageSrc(userData?.profileImageUrl);
-  }, [userData?.profileImageUrl]);
+  const {
+    data: userData,
+    isLoading,
+    isSuccess,
+  } = useQuery<UserData | undefined>("userProfile", () => getUser(), {
+    retry: 2,
+  });
+
+  const { mutateAsync: mutateUserProfile, isLoading: isUpdating } =
+    useMutation(editProfile);
 
   useEffect(() => {
-    setLocalName(userData?.username);
-  }, [userData?.username]);
+    if (isSuccess) {
+      setLocalProfileData({
+        username: userData?.username,
+        shortBio: userData?.shortBio,
+        job: userData?.job,
+        profileImageUrl: userData?.profileImageUrl,
+      });
+    }
+  }, [isSuccess]);
 
-  useEffect(() => {
-    setLocalBio(userData?.shortBio);
-  }, [userData?.shortBio]);
-
-  useEffect(() => {
-    setLocalJob(userData?.job);
-  }, [userData?.job]);
-
-  const uploadFile = () => {
-    const formData = new FormData();
-    // formData.append('profileImageFile')
-    // state.name && formData.append("username", state.name);
+  const updateProfile = async () => {
+    const trimedProfileData = Object.keys(localProfileData).reduce(
+      (acc, curr) => {
+        if (
+          typeof localProfileData[curr] === "string" &&
+          localProfileData[curr] !== "profileImageUrl"
+        ) {
+          acc[curr] = localProfileData[curr].trim();
+        } else {
+          acc[curr] = localProfileData[curr];
+        }
+        return acc;
+      },
+      {}
+    );
+    const editedProfileData: ProfileData = diff(userData, trimedProfileData);
+    console.log(_.isEqual(editedProfileData, {}));
+    if (_.isEqual(editedProfileData, {}))
+      return toast.info("프로필을 수정해주세요");
+    const { data } = await mutateUserProfile({
+      ...editedProfileData,
+    });
+    if (!data.ok) return alert(data.error);
+    toast.success("프로필 편집 성공했습니다");
+    history.push(routes.myPage);
   };
-
-  const errorMessages: string[] = [
-    "20자 이하의 이름을 입력해주세요",
-    "1자에서 8자 이내로 입력해주세요",
-    "1자 이상의 자기소개를 입력해주세요",
-  ];
 
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length <= 0 || e.target.value.length >= 20) {
       setLocalValidation([false, localValidation[1], localValidation[2]]);
+    } else {
+      setLocalValidation([true, localValidation[1], localValidation[2]]);
     }
-    setLocalName(e.target.value);
+    setLocalProfileData((prev) => ({
+      ...prev,
+      username: e.target.value,
+    }));
   };
 
   const handleBioChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     if (e.target.value.length <= 1) {
       setLocalValidation([localValidation[0], false, localValidation[2]]);
+    } else {
+      setLocalValidation([localValidation[0], true, localValidation[2]]);
     }
-    setLocalBio(e.target.value);
+    setLocalProfileData((prev) => ({
+      ...prev,
+      shortBio: e.target.value,
+    }));
   };
 
   const handleJobChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length <= 0 || e.target.value.length > 8) {
       setLocalValidation([localValidation[0], localValidation[1], false]);
+    } else {
+      setLocalValidation([localValidation[0], localValidation[1], true]);
     }
-    setLocalJob(e.target.value);
+    setLocalProfileData((prev) => ({
+      ...prev,
+      job: e.target.value,
+    }));
   };
 
   const handleFileOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -129,14 +157,22 @@ export default function EditProfilePage(props: Props) {
     const fileReader = new FileReader();
     fileReader.readAsDataURL(__file);
     fileReader.onload = (e) => {
-      setLocalImageSrc(e.target!.result as string);
-      // dispatch({
-      //   type: "setProfileImgUrl",
-      //   payload: e.target!.result as string,
-      // });
+      setLocalProfileData((prev) => ({
+        ...prev,
+        profileImageUrl: e.target!.result as string,
+      }));
     };
-    // dispatch({ type: "setProfileImgFile", payload: __file });
+    setLocalProfileData((prev) => ({
+      ...prev,
+      profileImageFile: __file,
+    }));
   };
+
+  const errorMessages: string[] = [
+    "20자 이하의 이름을 입력해주세요",
+    "1자 이상의 자기소개를 입력해주세요",
+    "1자에서 8자 이내로 입력해주세요",
+  ];
 
   return (
     <ContainerFlexColumn>
@@ -146,7 +182,9 @@ export default function EditProfilePage(props: Props) {
           <Heading>프로필 수정하기</Heading>
           <FileLabel htmlFor="input-file">
             <AvartarProfile
-              src={localImageSrc || "/avatar/anonymous_user.png"}
+              src={
+                localProfileData.profileImageUrl || "/avatar/anonymous_user.png"
+              }
             />
             <input
               id="input-file"
@@ -182,7 +220,7 @@ export default function EditProfilePage(props: Props) {
             <MidInput
               name="name"
               placeholder="Username"
-              value={localName || ""}
+              value={localProfileData.username || ""}
               onChange={handleNameChange}
               style={
                 localValidation[0]
@@ -200,7 +238,7 @@ export default function EditProfilePage(props: Props) {
               }}
             >
               <SmallInput
-                name="school"
+                name="university"
                 disabled
                 style={{ marginTop: "0px" }}
                 value={userData?.university || "학교"}
@@ -229,7 +267,7 @@ export default function EditProfilePage(props: Props) {
               name="selfpromotext"
               placeholder="ex. 미대에 다니는 다양한 삶을 살고 싶어하는 미개봉화석^^
             요즘 스타트업에 관심이 생겨서 관련하신 분들과 이야기하면 좋을 것 같아요ㅎㅎ"
-              value={localBio || ""}
+              value={localProfileData.shortBio || ""}
               onChange={handleBioChange}
               style={
                 localValidation[1]
@@ -244,7 +282,7 @@ export default function EditProfilePage(props: Props) {
             <MidInput
               name="job"
               placeholder="ex. 공대생 / 미개봉 새내기 / 디자이너"
-              value={localJob || ""}
+              value={localProfileData.job || ""}
               onChange={handleJobChange}
               style={
                 localValidation[2]
@@ -260,8 +298,27 @@ export default function EditProfilePage(props: Props) {
       </BackButtonLayout>
       <SpaceForNavBar></SpaceForNavBar>
       <BottomFixedButtonContainer>
-        <BottomFixedButtoninContainer>수정하기</BottomFixedButtoninContainer>
+        <BottomFixedButtoninContainer
+          disabled={diff(userData, localProfileData) === {}}
+          onClick={updateProfile}
+        >
+          수정하기
+        </BottomFixedButtoninContainer>
       </BottomFixedButtonContainer>
+      {isLoading ||
+        (isUpdating && (
+          <>
+            <LoaderBackdrop />
+            <LoaderWrapper>
+              <ClipLoader
+                loading={isLoading || isUpdating}
+                color={colors.MidBlue}
+                css={{ name: "width", styles: "border-width: 4px;" }}
+                size={40}
+              />
+            </LoaderWrapper>
+          </>
+        ))}
     </ContainerFlexColumn>
   );
 }
