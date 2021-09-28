06c2ed8db0f2c86be1b9bba5d4579f56fd5c8e44
정이든 <2donny@naver.com>
2021-09-28 21:15:04 +0900

    Refactoring: Reusability using selected props

--- src/components/shared/BottomNavBar.tsx
+++ src/components/shared/BottomNavBar.tsx
@@ -6,56 +6,41 @@ import {
 } from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+import { faUtensils, faStar, faUser } from "@fortawesome/free-solid-svg-icons";
 import routes from "../../routes";
 
 interface Props {
   selectedItem: string;
 }
 
-export default function BottomNavBar(props: Props) {
+export default function BottomNavBar({ selectedItem }: Props) {
   return (
     <>
       <BottomNavBarContainer>
         <Link to={routes.placeFeed} style={{ textDecoration: "none" }}>
-          {props.selectedItem === "places" ? (
-            <BottomNavBarItem style={{ color: colors.MidBlue }}>
-              <FontAwesomeIcon icon={faUtensils} />
-              써클
-            </BottomNavBarItem>
-          ) : (
-            <BottomNavBarItem style={{ color: colors.Black }}>
-              써클
-            </BottomNavBarItem>
-          )}
+          <BottomNavBarItem selected={selectedItem === "places"}>
+            <FontAwesomeIcon icon={faUtensils} />
+            <span className="pageName">써클</span>
+          </BottomNavBarItem>
         </Link>
+
         <Link
           to={routes.friend}
           style={{ textDecoration: "none", color: colors.Black }}
         >
-          {props.selectedItem === "friends" ? (
-            <BottomNavBarItem style={{ color: colors.MidBlue }}>
-              친구들
-            </BottomNavBarItem>
-          ) : (
-            <BottomNavBarItem style={{ color: colors.Black }}>
-              친구들
-            </BottomNavBarItem>
-          )}
+          <BottomNavBarItem selected={selectedItem === "friends"}>
+            <FontAwesomeIcon icon={faStar} />
+            <span className="pageName">친구들</span>
+          </BottomNavBarItem>
         </Link>
         <Link
           to={routes.myPage}
           style={{ textDecoration: "none", color: colors.Black }}
         >
-          {props.selectedItem === "mypage" ? (
-            <BottomNavBarItem style={{ color: colors.MidBlue }}>
-              마이페이지
-            </BottomNavBarItem>
-          ) : (
-            <BottomNavBarItem style={{ color: colors.Black }}>
-              마이페이지
-            </BottomNavBarItem>
-          )}
+          <BottomNavBarItem selected={selectedItem === "mypage"}>
+            <FontAwesomeIcon icon={faUser} />
+            <span className="pageName">마이페이지</span>
+          </BottomNavBarItem>
         </Link>
       </BottomNavBarContainer>
       <SpaceForNavBar />
@@ -63,13 +48,22 @@ export default function BottomNavBar(props: Props) {
   );
 }
 
-const BottomNavBarItem = styled.div`
+const BottomNavBarItem = styled.div<{ selected: boolean }>`
   width: 125px;
-  height: 75px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  color: ${(props) => (props.selected ? colors.MidBlue : "gray")};
+  .pageName {
+    font-size: 12px;
+    color: ${(props) => (props.selected ? colors.MidBlue : "gray")};
+    opacity: ${(props) => (props.selected ? 1 : 0)};
+  }
+  svg {
+    margin-bottom: 5px;
+    font-size: 20px;
+  }
   &:hover {
     opacity: 0.5;
     cursor: pointer;
