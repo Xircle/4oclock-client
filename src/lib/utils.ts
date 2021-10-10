8f25dd02f417218a201eb85298b5f257006cb906
정이든 <2donny@naver.com>
2021-10-10 18:30:50 +0900

    helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -63,22 +63,6 @@ export function participantsNumberLimit(time: number): string {
   }
 }
 
-export const getScrollTop = () => {
-  if (!document.body) return 0;
-  const scrollTop = document.documentElement
-    ? document.documentElement.scrollTop || document.body.scrollTop
-    : document.body.scrollTop;
-  return scrollTop;
-};
-
-export const getScrollBottom = () => {
-  if (!document.body) return 0;
-  const { scrollHeight } = document.body;
-  const { innerHeight } = window;
-  const scrollTop = getScrollTop();
-  return scrollHeight - innerHeight - scrollTop;
-};
-
 export const encodeUrlSlug = (url: string): string => {
   return url.replaceAll(" ", "-");
 };
