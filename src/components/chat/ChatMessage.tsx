663ddeb981f8a70f3cfeb1df397158e6a936f6ce
dja1716 <hdum@uwaterloo.ca>
2021-10-15 15:54:56 +0900

    fetch: Chatting date and time logic added

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,15 +1,24 @@
 import styled from "styled-components";
 import { IMessage } from "../../lib/api/types";
+import { ConvertSentTime } from "../../lib/utils";
+import moment from "moment";
 
 interface Props extends IMessage {}
 
 export default function ChatMessage({ isMe, isRead, content }: Props) {
+  const tempDate = new Date("2021-10-14T03:24:00");
   return (
     <Container isMe={isMe}>
       <MessageContainer isMe={isMe}>{content}</MessageContainer>
+      <TimeText>{ConvertSentTime(tempDate)}</TimeText>
     </Container>
   );
 }
+
+const TimeText = styled.div`
+  color: #a7b0c0;
+  font-size: 13px;
+`;
 
 const MessageContainer = styled.div<{ isMe?: boolean }>`
   max-width: 60%;
