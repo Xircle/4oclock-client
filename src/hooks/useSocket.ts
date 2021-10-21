f5a17f9a6fd424bbd6e198222d0d32a998317f58
정이든 <2donny@naver.com>
2021-10-21 17:33:12 +0900

    url 변경

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -8,7 +8,7 @@ import storage from "../lib/storage";
 const socketServerUrl =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_TEST_API_SERVER
-    : process.env.REACT_APP_TEST_API_SERVER;
+    : "http://localhost:3080";
 
 const existingSockets: { [key: string]: Socket } = {};
 
