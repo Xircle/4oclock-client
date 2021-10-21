84db9296f7cdeb55d6c16ec53718a3cb654d171e
정이든 <2donny@naver.com>
2021-10-21 17:34:35 +0900

    chat 관련 helper함수 추가

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,4 +1,15 @@
+import { IRoom } from "./api/types";
+import storage from "./storage";
+
 export function parseHashTags(string): string {
   const parsedString: string[] = JSON.parse(string);
   return "#" + parsedString.join(" #");
 }
+
+export const SetLocalStorageItemWithMyRoom = (myRooms: IRoom[]) => {
+  for (let myRoom of myRooms) {
+    if (!storage.getItem(`chat-${myRoom.receiver.id}`)) {
+      storage.setItem(`chat-${myRoom.receiver.id}`, myRoom.id);
+    }
+  }
+};
