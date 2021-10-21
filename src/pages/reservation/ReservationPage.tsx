42b6e310b0e4a7936e83ed57721c3f949af1e6e6
dja1716 <hdum@uwaterloo.ca>
2021-10-21 17:45:38 +0900

    update: vaccine wording

--- src/pages/reservation/ReservationPage.tsx
+++ src/pages/reservation/ReservationPage.tsx
@@ -139,13 +139,9 @@ export default function ReservationPage({ match, location, history }: Props) {
               <FontAwesomeIcon icon={faCircle} color={colors.LightGray} />
             )}
             <span>
-              [백신 접종 여부 체크] 2차 <span className="info"> 백신 접종</span>
-              을 받았습니다.
+              <span className="info">[백신 접종] 2차 백신 접종 후 14일이 지났습니다</span>
             </span>
           </Row>
-          <VaccineInfoText>
-            접종 후 14일이 지난 분만 선택해주세요
-          </VaccineInfoText>
           <Instruction>
             <InstructionHeading>💖 연고이팅 이용가이드 💖</InstructionHeading>
             <InstructionDetail>
