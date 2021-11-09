732f9905ac0f1d843f04a1d48cccae6500a9b493
정이든 <2donny@naver.com>
2021-11-09 20:05:18 +0900

    hide S3 origin

--- src/lib/optimizeImage.ts
+++ src/lib/optimizeImage.ts
@@ -1,12 +1,11 @@
-// 추후에 이미지 최적할 때 쓰기
 const CDN_IMAGE_DOMAIN = "https://dh2n68u8fomh1.cloudfront.net";
-const IMAGE_ORIGIN =
-  "https://xircle-profile-upload.s3.ap-northeast-2.amazonaws.com";
+const IMAGE_ORIGIN = process.env.REACT_APP_IMAGE_ORIGIN;
+
 export default function optimizeImage(url: string, width?: number): string {
-  if (!url.includes(IMAGE_ORIGIN)) return url;
+  if (!url.includes(IMAGE_ORIGIN!)) return url;
   if (url.includes(".svg")) return url;
 
-  let replaced = url.replace(IMAGE_ORIGIN, CDN_IMAGE_DOMAIN); // Cloudfront
+  let replaced = url.replace(IMAGE_ORIGIN!, CDN_IMAGE_DOMAIN); // Cloudfront
 
   if (!width) {
     return replaced;
