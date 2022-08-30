8c2ab3d458b5eb3bf7d73271d064c986b1f0808e
dja1716 <hdum@uwaterloo.ca>
2022-08-30 17:41:25 +0900

    edit: PaymentPage

--- src/pages/payment/PaymentPage.tsx
+++ src/pages/payment/PaymentPage.tsx
@@ -1,7 +1,13 @@
 import { RouteComponentProps } from "react-router-dom";
 import styled from "styled-components";
 import { MainBtn } from "../../styles/styles";
+import { loadTossPayments } from "@tosspayments/payment-sdk";
+const clientKey = "test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq";
 
+// async/await을 사용하는 경우
+async function main() {
+  const tossPayments = await loadTossPayments(clientKey);
+}
 interface Props extends RouteComponentProps {}
 
 export default function PaymentPage({ history, location }: Props) {
