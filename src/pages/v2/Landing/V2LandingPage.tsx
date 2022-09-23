32163d3d67d1d685987e08e56cbb5145c1af729a
dja1716 <hdum@uwaterloo.ca>
2022-09-23 17:32:24 +0900

    edit: V2LandingPage

--- src/pages/v2/Landing/V2LandingPage.tsx
+++ src/pages/v2/Landing/V2LandingPage.tsx
@@ -1,7 +1,38 @@
-import { Container } from "../../../styles/styles";
+import { faBars, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import { colors, Container } from "../../../styles/styles";
 
 function V2LandingPage() {
-  return <Container></Container>;
+  return (
+    <SContainer>
+      <Header>
+        <HeaderItem>
+          <FontAwesomeIcon icon={faBars} color={colors.Black} size="2x" />
+        </HeaderItem>
+        <HeaderItem>로그인</HeaderItem>
+      </Header>
+    </SContainer>
+  );
 }
+
+const SContainer = styled(Container)`
+  background-color: red;
+  min-height: 100vh;
+`;
+
+const Header = styled.div`
+  position: fixed;
+  width: 375px;
+  height: 80px;
+  background-color: ${colors.LightGray};
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 10px;
+`;
+
+const HeaderItem = styled.div`
+  align-self: center;
+`;
 
 export default V2LandingPage;
