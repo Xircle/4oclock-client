badcc087a36e8b769ef9ef5ea27d27afa0e8ea53
dja1716 <hdum@uwaterloo.ca>
2022-09-23 21:11:59 +0900

    edit: V2LandingPage

--- src/pages/v2/Landing/V2LandingPage.tsx
+++ src/pages/v2/Landing/V2LandingPage.tsx
@@ -1,12 +1,37 @@
 import { faBars, faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Drawer } from "@material-ui/core";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useQuery } from "react-query";
 import styled from "styled-components";
+import { seeAllCategory } from "../../../lib/api/seeAllCategory";
+import { CategoryData } from "../../../lib/api/types";
 import { colors, Container } from "../../../styles/styles";
 
 function V2LandingPage() {
   const [drawerOpened, setDrawerOpened] = useState(false);
+  const [categories, setCategories] = useState<CategoryData[]>([]);
+
+  const { data: categoryData } = useQuery<CategoryData[] | undefined>(
+    ["place-detail"],
+    () => seeAllCategory(),
+    {
+      onError: (err: any) => {
+        alert(err);
+        return;
+      },
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  useEffect(() => {
+    if (categoryData !== undefined && categoryData.length > 0) {
+      setCategories(categoryData);
+    }
+    console.log(categoryData);
+  }, [categoryData]);
+
   return (
     <SContainer>
       <Header>
@@ -20,10 +45,11 @@ function V2LandingPage() {
         <HeaderItem>로그인</HeaderItem>
       </Header>
       <Body>
-        <HeaderItem>로그인</HeaderItem>
-        <HeaderItem>로그인</HeaderItem>
-        <HeaderItem>로그인</HeaderItem>
-        <HeaderItem>로그인</HeaderItem>
+        <FilterContainer>
+          {categories.map((item) => {
+            return <FilterOption key={item.id}>{item.name}</FilterOption>;
+          })}
+        </FilterContainer>
       </Body>
       <Drawer open={drawerOpened} onClose={() => setDrawerOpened(false)}>
         <DrawerItem>내 지원서 보기</DrawerItem>
@@ -34,12 +60,33 @@ function V2LandingPage() {
   );
 }
 
+const FilterContainer = styled.div`
+  width: 100%;
+  padding-left: 5px;
+  padding-right: 5px;
+`;
+
+const FilterWrapper = styled.div``;
+
+const FilterOption = styled.span`
+  margin: 10px;
+  background-color: #fff8d1;
+  padding-left: 7px;
+  padding-right: 7px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  border-radius: 6px;
+  cursor: pointer;
+  display: inline-block;
+`;
+
 const SContainer = styled(Container)`
   min-height: 100vh;
 `;
 
 const Body = styled.div`
   min-height: 300vh;
+  padding-top: 30px;
 `;
 
 const DrawerItem = styled.div`
@@ -53,11 +100,12 @@ const Header = styled.div`
   position: sticky;
   width: 375px;
   height: 80px;
-  background-color: ${colors.LightGray};
   display: flex;
   justify-content: space-between;
   padding: 0px 10px;
   top: 0px;
+  -webkit-box-shadow: 0px 12px 13px -9px rgba(0, 0, 0, 0.43);
+  box-shadow: 0px 12px 13px -9px rgba(0, 0, 0, 0.43);
 `;
 
 const HeaderItem = styled.div`
