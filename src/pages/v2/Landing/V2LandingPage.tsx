5d247528fa7e9a48c3e6e3f7d5a8e517591be2c5
dja1716 <hdum@uwaterloo.ca>
2022-09-23 17:43:55 +0900

    edit: V2LandingPage

--- src/pages/v2/Landing/V2LandingPage.tsx
+++ src/pages/v2/Landing/V2LandingPage.tsx
@@ -12,23 +12,34 @@ function V2LandingPage() {
         </HeaderItem>
         <HeaderItem>로그인</HeaderItem>
       </Header>
+      <Body>
+        <HeaderItem>로그인</HeaderItem>
+        <HeaderItem>로그인</HeaderItem>
+        <HeaderItem>로그인</HeaderItem>
+        <HeaderItem>로그인</HeaderItem>
+      </Body>
     </SContainer>
   );
 }
 
 const SContainer = styled(Container)`
-  background-color: red;
   min-height: 100vh;
 `;
 
+const Body = styled.div`
+  background-color: blue;
+  min-height: 300vh;
+`;
+
 const Header = styled.div`
-  position: fixed;
+  position: sticky;
   width: 375px;
   height: 80px;
   background-color: ${colors.LightGray};
   display: flex;
   justify-content: space-between;
   padding: 0px 10px;
+  top: 0px;
 `;
 
 const HeaderItem = styled.div`
