07097df8ade2420309effc39fe0ad2df2737171b
dja1716 <hdum@uwaterloo.ca>
2022-09-27 18:14:17 +0900

    edit: V2MyPage

--- src/pages/v2/MyPage/V2MyPage.tsx
+++ src/pages/v2/MyPage/V2MyPage.tsx
@@ -1,3 +1,10 @@
+import V2HeaderC from "../../../components/V2/UI/V2HeaderC";
+import { Container } from "../../../styles/styles";
+
 export default function V2MyPage() {
-  return <div></div>;
+  return (
+    <Container>
+      <V2HeaderC />
+    </Container>
+  );
 }
