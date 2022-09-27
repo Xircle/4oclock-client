87288de40371bea7574f7ddb619e8d19855ac9c9
dja1716 <hdum@uwaterloo.ca>
2022-09-27 18:58:46 +0900

    edit: V2MyPage

--- src/pages/v2/MyPage/V2MyPage.tsx
+++ src/pages/v2/MyPage/V2MyPage.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import V2HeaderC from "../../../components/V2/UI/V2HeaderC";
 import { Container } from "../../../styles/styles";
 
@@ -5,6 +6,23 @@ export default function V2MyPage() {
   return (
     <Container>
       <V2HeaderC title="my page" />
+      <Body>
+        <BodyItem>
+          <BodyItemHeading>승인 대기중</BodyItemHeading>
+        </BodyItem>
+        <BodyItem>
+          <BodyItemHeading>승인된 모임</BodyItemHeading>
+        </BodyItem>
+        <BodyItem>
+          <BodyItemHeading>회원 정보</BodyItemHeading>
+        </BodyItem>
+      </Body>
     </Container>
   );
 }
+
+const Body = styled.div``;
+
+const BodyItem = styled.div``;
+
+const BodyItemHeading = styled.div``;
