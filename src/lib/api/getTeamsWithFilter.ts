b4fe02e6e5674aa623eccf96ffabbb8f3e5be516
dja1716 <hdum@uwaterloo.ca>
2022-09-29 16:35:40 +0900

    edit: getTeamsWithFilter

--- src/lib/api/getTeamsWithFilter.ts
+++ src/lib/api/getTeamsWithFilter.ts
@@ -1,12 +1,11 @@
 import AxiosClient from "../apiClient";
 
-export const seeTeamsWithFilter = async (
-  page: number = 1,
-  limit: number = 10,
-) => {
-  const { data } = await AxiosClient.get(
-    `team/all/filter?page=${page}&limit=${limit}`,
-  );
+export const seeTeamsWithFilter = async (page: number = 0) => {
+  const { data } = await AxiosClient.get(`team/all/filter`, {
+    params: {
+      page: page,
+    },
+  });
 
   if (!data.ok) {
     throw new Error(data.error);
