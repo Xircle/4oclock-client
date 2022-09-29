26924e689c315f330847ae112d7012b80cc1538a
dja1716 <hdum@uwaterloo.ca>
2022-09-29 18:23:29 +0900

    edit: TeamFeedRenderItem

--- src/components/V2/Team/TeamFeedRenderItem.tsx
+++ src/components/V2/Team/TeamFeedRenderItem.tsx
@@ -1,8 +1,17 @@
+import styled from "styled-components";
+
 interface Props {
   image: string;
   name: string;
+  key: string;
 }
 
 export default function TeamFeedRenderItem(props: Props) {
-  return <div></div>;
+  return <Conatiner></Conatiner>;
 }
+
+const Conatiner = styled.div`
+  width: 100%;
+  min-height: 100px;
+  margin-bottom: 10px;
+`;
