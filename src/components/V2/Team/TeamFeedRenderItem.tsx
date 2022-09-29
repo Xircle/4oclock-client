fecc39c1a9fa17e52471294d282801dc27cb020c
dja1716 <hdum@uwaterloo.ca>
2022-09-29 19:35:28 +0900

    edit: TeamFeedRenderItem

--- src/components/V2/Team/TeamFeedRenderItem.tsx
+++ src/components/V2/Team/TeamFeedRenderItem.tsx
@@ -7,12 +7,32 @@ interface IProps {
 }
 
 export default function TeamFeedRenderItem({ image, name, key }: IProps) {
-  return <Conatiner>{name}</Conatiner>;
+  return (
+    <Conatiner>
+      <LeftContainer>{name}</LeftContainer>
+      <RightContainer>{name}</RightContainer>
+    </Conatiner>
+  );
 }
 
 const Conatiner = styled.div`
   width: 100%;
   min-height: 100px;
-  margin-bottom: 10px;
   background-color: red;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LeftContainer = styled.div`
+  background-color: green;
+
+  width: 245px;
+`;
+
+const RightContainer = styled.div`
+  background-color: black;
+  width: 100px;
+  height: 100px;
 `;
