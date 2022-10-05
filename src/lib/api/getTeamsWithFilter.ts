e7bd221057a3e7acf81455e72aa4464614d71d2e
dja1716 <hdum@uwaterloo.ca>
2022-10-05 22:34:52 +0900

    edit: getTeamsWithFilter

--- src/lib/api/getTeamsWithFilter.ts
+++ src/lib/api/getTeamsWithFilter.ts
@@ -1,10 +1,11 @@
-import { TimeData } from "./../v2/utils";
+import { AgeData, TimeData } from "./../v2/utils";
 import { CategoryData } from "./types.d";
 import AxiosClient from "../apiClient";
 
 export const seeTeamsWithFilter = async (
   categories: CategoryData[],
   times: TimeData[],
+  ageData: AgeData[],
   page: number = 0,
 ) => {
   let categoryQuery: string[] = [];
@@ -19,12 +20,28 @@ export const seeTeamsWithFilter = async (
       timeQuery.push(time.numV);
     }
   });
+  let minAge = 100;
+  let maxAge = 0;
+  ageData.map((age) => {
+    if (age.selected) {
+      if (minAge > age.minAge) {
+        minAge = age.minAge;
+      }
+      if (maxAge < age.maxAge) {
+        maxAge = age.maxAge;
+      }
+    }
+  });
+
+  console.log(minAge + " " + maxAge);
 
   const { data } = await AxiosClient.get(`team/all/filter`, {
     params: {
       page: page,
       categoryIds: categoryQuery,
       times: timeQuery,
+      minAge: minAge < 100 ? minAge : 20,
+      maxAge: maxAge > 0 ? maxAge : 30,
     },
   });
 
