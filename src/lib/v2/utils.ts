5cbf874ee1fc84556239a6f550ae1c9c074b9881
dja1716 <hdum@uwaterloo.ca>
2022-10-05 16:13:00 +0900

    edit: utils

--- src/lib/v2/utils.ts
+++ src/lib/v2/utils.ts
@@ -1,9 +1,27 @@
-export type DayNumToKor = {
-  0: "일";
-  1: "월";
-  2: "화";
-  3: "수";
-  4: "목";
-  5: "금";
-  6: "토";
+export const DayNumToKor = (day: number) => {
+  let result;
+  switch (day) {
+    case 0:
+      result = "일";
+      break;
+    case 1:
+      result = "월";
+      break;
+    case 2:
+      result = "화";
+      break;
+    case 3:
+      result = "수";
+      break;
+    case 4:
+      result = "목";
+      break;
+    case 5:
+      result = "금";
+      break;
+    case 6:
+      result = "토";
+      break;
+  }
+  return result;
 };
