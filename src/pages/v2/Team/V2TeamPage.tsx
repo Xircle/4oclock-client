6a1468c443607c93c792031d37949d307ff34ae5
dja1716 <hdum@uwaterloo.ca>
2022-10-07 20:39:49 +0900

    V2TeamPage

--- src/pages/v2/Team/V2TeamPage.tsx
+++ src/pages/v2/Team/V2TeamPage.tsx
@@ -1,7 +1,31 @@
+import { useQuery } from "react-query";
+import { RouteComponentProps } from "react-router-dom";
 import styled from "styled-components";
+import { getTeamById } from "../../../lib/api/getTeamById";
+import { TeamData } from "../../../lib/api/types";
+interface MatchParms {
+  teamId: string;
+}
+interface LocationState {}
 
-export default function V2TeamPage() {
-  return <Container>TeamPage</Container>;
+interface Props extends RouteComponentProps<MatchParms, {}, LocationState> {}
+
+export default function V2TeamPage({ match, location }: Props) {
+  const { teamId } = match.params;
+  const { data: teamData } = useQuery<TeamData | undefined>(
+    ["team", teamId],
+    () => getTeamById(teamId),
+    {
+      onError: (err: any) => {
+        alert(err);
+        return;
+      },
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return <Container>{teamData?.name}</Container>;
 }
 
 const Container = styled.div``;
