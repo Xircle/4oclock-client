3a0f52f8112e7d15c66441dde2410deb40d53685
dja1716 <hdum@uwaterloo.ca>
2022-10-07 20:45:57 +0900

    edit: V2TeamPage

--- src/pages/v2/Team/V2TeamPage.tsx
+++ src/pages/v2/Team/V2TeamPage.tsx
@@ -3,6 +3,7 @@ import { RouteComponentProps } from "react-router-dom";
 import styled from "styled-components";
 import { getTeamById } from "../../../lib/api/getTeamById";
 import { TeamData } from "../../../lib/api/types";
+import optimizeImage from "../../../lib/optimizeImage";
 interface MatchParms {
   teamId: string;
 }
@@ -25,7 +26,30 @@ export default function V2TeamPage({ match, location }: Props) {
     },
   );
 
-  return <Container>{teamData?.name}</Container>;
+  return (
+    <Container>
+      <SHeader>
+        <SHeaderPic
+          src={optimizeImage(teamData?.images?.[0] || "hi", {
+            width: 375,
+            height: 230,
+          })}
+          alt={teamData && teamData?.name + "사진"}
+        />
+      </SHeader>
+    </Container>
+  );
 }
 
 const Container = styled.div``;
+
+const SHeader = styled.div`
+  height: 160px;
+  position: relative;
+`;
+
+const SHeaderPic = styled.img`
+  width: 100%;
+  height: 160px;
+  object-fit: cover;
+`;
