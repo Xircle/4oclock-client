0b863a0500b9479e6e845c2ef9480754400c52c7
dja1716 <hdum@uwaterloo.ca>
2022-10-11 13:12:05 +0900

    edit: V2ApplyingPage

--- src/pages/v2/Applying/V2ApplyingPage.tsx
+++ src/pages/v2/Applying/V2ApplyingPage.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 import V2SubHeaderC from "../../../components/V2/UI/V2SubHeaderC";
 
-interface Props {}
+interface Props {
+  clubName?: string;
+  meeting_hour?: number;
+  meeting_day?: number;
+  price?: number;
+}
 
-export default function V2ApplyingPage({}: Props) {
+export default function V2ApplyingPage({
+  clubName,
+  meeting_day,
+  meeting_hour,
+  price,
+}: Props) {
   return (
     <Container>
       <V2SubHeaderC title="정모 활동 정보" />
