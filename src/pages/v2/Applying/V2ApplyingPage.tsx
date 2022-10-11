a46e6716962e616ba030d0d196d1aedb69472270
dja1716 <hdum@uwaterloo.ca>
2022-10-11 12:59:56 +0900

    edit: V2ApplyingPage

--- src/pages/v2/Applying/V2ApplyingPage.tsx
+++ src/pages/v2/Applying/V2ApplyingPage.tsx
@@ -1,3 +1,14 @@
-export default function V2ApplyingPage() {
-  return <div>hi</div>;
+import styled from "styled-components";
+import V2SubHeaderC from "../../../components/V2/UI/V2SubHeaderC";
+
+interface Props {}
+
+export default function V2ApplyingPage({}: Props) {
+  return (
+    <Container>
+      <V2SubHeaderC title="정모 활동 정보" />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
