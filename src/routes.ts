15984e2478c33577dc8fc726cfad9b930eefe767
dja1716 <hdum@uwaterloo.ca>
2022-10-11 13:27:20 +0900

    edit: routes

--- src/routes.ts
+++ src/routes.ts
@@ -30,7 +30,7 @@ const routes = {
   socialRedirect: "/v2/social/redirect",
   v2MyPage: "/v2/mypage",
   v2TeamPage: "/v2/team/:teamId",
-  v2ApplyPage: "/v2/apply",
+  v2ApplyPage: "/v2/apply/:teamId",
 };
 
 export default routes;
