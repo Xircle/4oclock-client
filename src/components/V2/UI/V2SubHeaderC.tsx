0d15ad04f6d7b5760ddb4cac0bf157edaf11671b
dja1716 <hdum@uwaterloo.ca>
2022-10-11 12:59:43 +0900

    edit: V2SubHeaderC

---
+++ src/components/V2/UI/V2SubHeaderC.tsx
@@ -0,0 +1,46 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { RouteComponentProps, useHistory } from "react-router-dom";
+import styled from "styled-components";
+
+interface LocationState {}
+
+interface Props {
+  title: string;
+}
+
+export default function V2SubHeaderC({ title }: Props) {
+  const history = useHistory();
+  return (
+    <Header>
+      <SFontAwesomeIcon
+        onClick={() => history.goBack()}
+        icon={faArrowLeft}
+        size="lg"
+        color="black"
+      />
+      <HeaderText>{title}</HeaderText>
+    </Header>
+  );
+}
+
+const SFontAwesomeIcon = styled(FontAwesomeIcon)`
+  cursor: pointer;
+  position: absolute;
+  left: 5px;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 50px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderText = styled.div`
+  color: #505050;
+  font-weight: 700;
+  font-size: 20px;
+`;
