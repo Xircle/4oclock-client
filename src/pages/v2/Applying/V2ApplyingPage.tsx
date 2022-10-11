9305a03c6690035d110ac51ecd1cb5f19e6d31d5
dja1716 <hdum@uwaterloo.ca>
2022-10-11 15:47:50 +0900

    edit: V2ApplyingPage

--- src/pages/v2/Applying/V2ApplyingPage.tsx
+++ src/pages/v2/Applying/V2ApplyingPage.tsx
@@ -29,9 +29,49 @@ export default function V2ApplyingPage({ match, location }: Props) {
 
   return (
     <Container>
-      <V2SubHeaderC title={clubName} />
+      <V2SubHeaderC title={"정모 신청서 작성"} />
+      <InfoContainer>
+        <Title>{clubName}</Title>
+        <InfoRow>
+          <InfoQuestion>모임시간</InfoQuestion>
+          <InforAnswer>매주 금요일 6시</InforAnswer>
+        </InfoRow>
+        <InfoRow>
+          <InfoQuestion>모임시간</InfoQuestion>
+          <InforAnswer>매주 금요일 6시</InforAnswer>
+        </InfoRow>
+        <InfoRow>
+          <InfoQuestion>모임시간</InfoQuestion>
+          <InforAnswer>매주 금요일 6시</InforAnswer>
+        </InfoRow>
+      </InfoContainer>
     </Container>
   );
 }
 
 const Container = styled.div``;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+  color: #505050;
+`;
+
+const InfoContainer = styled.div`
+  padding: 26px;
+`;
+
+const InfoRow = styled.div`
+  display: flex;
+  margin-top: 20px;
+  text-align: center;
+`;
+
+const InfoQuestion = styled.div`
+  font-weight: 500;
+  font-size: 18px;
+  color: #6f7789;
+  width: 100px;
+`;
+
+const InforAnswer = styled.div``;
