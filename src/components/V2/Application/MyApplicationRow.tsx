336c5d6f62b5a73a101f80d0ca3dec29c08cc0f0
dja1716 <hdum@uwaterloo.ca>
2022-10-12 13:15:17 +0900

    edit: MyApplicationRow

--- src/components/V2/Application/MyApplicationRow.tsx
+++ src/components/V2/Application/MyApplicationRow.tsx
@@ -1,9 +1,35 @@
 import styled from "styled-components";
 
-interface IProps {}
-
-export default function MyApplicationRow({}: IProps) {
-  return <Container>hi</Container>;
+interface IProps {
+  id: string;
+  teamId: number;
+  paid?: boolean;
+  teamImage: string;
+  status: string;
 }
 
-const Container = styled.div``;
+export default function MyApplicationRow({}: IProps) {
+  return (
+    <Container>
+      <LeftContainer></LeftContainer>
+      <RightContainer></RightContainer>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  background-color: black;
+  margin-bottom: 10px;
+`;
+
+const LeftContainer = styled.div`
+  background-color: red;
+  width: 80px;
+`;
+const RightContainer = styled.div`
+  height: 100%;
+  background-color: blue;
+`;
