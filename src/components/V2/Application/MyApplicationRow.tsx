df1860b607bb6fba5019bf533719208f1a47e56c
dja1716 <hdum@uwaterloo.ca>
2022-10-12 12:37:27 +0900

    edit: My Application row

---
+++ src/components/V2/Application/MyApplicationRow.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+interface IProps {}
+
+export default function MyApplicationRow({}: IProps) {
+  return <Container>hi</Container>;
+}
+
+const Container = styled.div``;
