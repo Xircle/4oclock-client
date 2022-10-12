e80b8b3f3ea091878594c9d91b37197496446a86
dja1716 <hdum@uwaterloo.ca>
2022-10-12 18:49:14 +0900

    edit: V2MyPage

--- src/pages/v2/MyPage/V2MyPage.tsx
+++ src/pages/v2/MyPage/V2MyPage.tsx
@@ -86,7 +86,7 @@ const BodyItem = styled.div`
 const BodyScroll = styled.div`
   width: 100%;
   height: 150px;
-  overflow: scroll;
+  overflow-y: scroll;
 `;
 
 const BodyItemHeading = styled.div`
