4fb6774220fa443264595869e241dccc9fda3e7a
dja1716 <hdum@uwaterloo.ca>
2022-10-12 13:52:06 +0900

    edit: MyApplicationRow

--- src/components/V2/Application/MyApplicationRow.tsx
+++ src/components/V2/Application/MyApplicationRow.tsx
@@ -6,15 +6,33 @@ interface IProps {
   paid?: boolean;
   teamImage: string;
   status: string;
+  teamName: string;
+  refetch: () => Promise<void>;
 }
 
-export default function MyApplicationRow({ teamImage }: IProps) {
+export default function MyApplicationRow({
+  teamImage,
+  teamName,
+  status,
+  refetch,
+}: IProps) {
+  const cancelCTA = async () => {
+    await refetch();
+  };
+
   return (
     <Container>
       <LeftContainer>
         <TeamImage src={teamImage} />
       </LeftContainer>
-      <RightContainer></RightContainer>
+      <RightContainer>
+        <TeamNameTag>{teamName}</TeamNameTag>
+        {status === "Pending" && (
+          <CTAButton style={{ backgroundColor: "#C4CBD8" }}>
+            신청 취소하기
+          </CTAButton>
+        )}
+      </RightContainer>
     </Container>
   );
 }
@@ -40,4 +58,21 @@ const LeftContainer = styled.div`
 `;
 const RightContainer = styled.div`
   height: 100%;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const TeamNameTag = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+`;
+
+const CTAButton = styled.div`
+  border-radius: 3px;
+  font-weight: 500;
+  font-size: 11px;
+  padding: 5px 3px;
+  color: #ffffff;
 `;
