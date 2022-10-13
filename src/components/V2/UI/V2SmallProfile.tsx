a5c9296e7beed53798f64672b353b2c60d1215b2
dja1716 <hdum@uwaterloo.ca>
2022-10-13 14:09:46 +0900

    edit: V2SmallProfile

--- src/components/V2/UI/V2SmallProfile.tsx
+++ src/components/V2/UI/V2SmallProfile.tsx
@@ -1,7 +1,62 @@
-interface Props {
-  title?: string;
+import styled from "styled-components";
+import optimizeImage from "../../../lib/optimizeImage";
+import storage from "../../../lib/storage";
+import { RoleHashTable } from "../../../lib/v2/utils";
+import { CURRENT_USER } from "../../shared/constants";
+
+interface Props {}
+
+export default function V2SmallProfile(props: Props) {
+  return (
+    <Container>
+      <LeftContainer>
+        <ProfileImg
+          src={optimizeImage(
+            storage.getItem(CURRENT_USER)?.profile?.thumbnail,
+            {
+              width: 80,
+              height: 80,
+            },
+          )}
+        />
+      </LeftContainer>
+      <RightContainer>
+        <NameTag>{storage.getItem(CURRENT_USER)?.username}</NameTag>
+        <RoleTag>
+          {RoleHashTable[storage.getItem(CURRENT_USER)?.profile?.role]}
+        </RoleTag>
+      </RightContainer>
+    </Container>
+  );
 }
 
-export default function V2SmallProfile() {
-  return <div></div>;
-}
+const Container = styled.div`
+  display: flex;
+`;
+
+const ProfileImg = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+`;
+
+const LeftContainer = styled.div``;
+
+const RightContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  padding-left: 20px;
+`;
+
+const NameTag = styled.div`
+  color: #50555c;
+  font-weight: 500;
+  font-size: 15px;
+`;
+
+const RoleTag = styled.div`
+  color: #fd8a66;
+  font-weight: 500;
+  font-size: 15px;
+`;
