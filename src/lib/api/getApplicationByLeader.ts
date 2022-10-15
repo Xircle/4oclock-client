c6289ee70aca2301073a16ab83324c8087f37e5f
dja1716 <hdum@uwaterloo.ca>
2022-10-15 13:21:07 +0900

    edit: getApplicationByLeader

--- src/lib/api/getApplicationByLeader.ts
+++ src/lib/api/getApplicationByLeader.ts
@@ -1,20 +1,32 @@
 import {
   GetApplicationByLeaderData,
   GetApplicationByLeaderOutput,
+  GetTeamApplicationInput,
 } from "./types.d";
 
 import AxiosClient from "../apiClient";
 
 export const getApplicationByLeader = async (
-  applicationId?: string,
+  getTeamApplicationInput: GetTeamApplicationInput,
 ): Promise<GetApplicationByLeaderData | undefined> => {
-  if (!applicationId) return;
+  const { param1, param2 } = getTeamApplicationInput;
+  console.log({ param1, param2 });
 
-  const { data } = await AxiosClient.get<GetApplicationByLeaderOutput>(
-    `application/leader?applicationId=${applicationId}`,
-  );
-  if (!data.ok) {
-    alert(data.error);
+  if (param1 && param2) {
+    const { data } = await AxiosClient.get<GetApplicationByLeaderOutput>(
+      `application/leader?userId=${param1}&teamId=${param2}`,
+    );
+    if (!data.ok) {
+      alert(data.error);
+    }
+    return data.data;
+  } else if (param1) {
+    const { data } = await AxiosClient.get<GetApplicationByLeaderOutput>(
+      `application/leader?applicationId=${param1}`,
+    );
+    if (!data.ok) {
+      alert(data.error);
+    }
+    return data.data;
   }
-  return data.data;
 };
