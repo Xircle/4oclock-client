ae1adbe0a94f50d5d187e2ff90bd89c662454a86
dja1716 <hdum@uwaterloo.ca>
2022-11-14 21:23:30 +0900

    edit: getMyApplications

--- src/lib/api/getMyApplications.ts
+++ src/lib/api/getMyApplications.ts
@@ -1,12 +1,16 @@
+import { checkResponseStatus } from "./../checkResponseStatus";
 import AxiosClient from "../apiClient";
 import { GetMyApplicationsOutput, MyApplicationsByStatus } from "./types";
 
-export const getMyApplications = async (): Promise<GetMyApplicationsOutput> => {
-  const { data } = await AxiosClient.get<GetMyApplicationsOutput>(
-    "user/history/applications",
-  );
-  if (!data.ok) {
-    alert(data.error);
-  }
-  return data;
-};
+export const getMyApplications =
+  async (): Promise<GetMyApplicationsOutput | void> => {
+    const response = await AxiosClient.get<GetMyApplicationsOutput>(
+      "user/history/applications",
+    );
+    if (!response.data.ok) {
+      alert(response.data.error);
+      checkResponseStatus(response.status);
+    } else {
+      return response.data;
+    }
+  };
