abed379119c07f23f4ddab95646d019566bd35fd
dja1716 <hdum@uwaterloo.ca>
2022-11-14 21:24:20 +0900

    edit: makeReservation

--- src/lib/api/makeReservation.ts
+++ src/lib/api/makeReservation.ts
@@ -1,12 +1,18 @@
+import { checkResponseStatus } from "./../checkResponseStatus";
 import { MakeReservationInput, MakeReservationOutput } from "./types.d";
 import { AxiosResponse } from "axios";
 import AxiosClient from "../apiClient";
 
 export const makeReservation = async (
-  makeReservationInput: MakeReservationInput
-): Promise<AxiosResponse<MakeReservationOutput>> => {
-  return AxiosClient.post<MakeReservationOutput>(
+  makeReservationInput: MakeReservationInput,
+): Promise<AxiosResponse<MakeReservationOutput> | void> => {
+  const response = await AxiosClient.post<MakeReservationOutput>(
     "reservation",
-    makeReservationInput
+    makeReservationInput,
   );
+  if (!response?.data?.ok) {
+    checkResponseStatus(response?.status);
+  } else {
+    return response;
+  }
 };
