02f242f32ac4d0d6bb0781f82cb5dc572ed009dd
dja1716 <hdum@uwaterloo.ca>
2022-11-16 20:03:04 +0900

    edit: searchSchool

--- src/lib/api/3rdApi/searchSchool.ts
+++ src/lib/api/3rdApi/searchSchool.ts
@@ -7,8 +7,6 @@ export const searchSchool = async (
   const thisPage = 1;
   const perPage = 20;
   const axios = require("axios");
-  console.log(apiKey);
-  console.log(searchSchuNm);
 
   const res = await axios.get(
     `https://www.career.go.kr/cnet/openapi/getOpenApi.json?apiKey=a959ed4f1f173b6a1756c8943ab55e94&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${thisPage}&perPage=${perPage}&searchSchulNm=${searchSchuNm}`,
