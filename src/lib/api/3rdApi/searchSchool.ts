6855a7a4d7eba6421038273324456e274ccfba79
dja1716 <hdum@uwaterloo.ca>
2022-11-16 19:57:18 +0900

    edit: searchSchool

--- src/lib/api/3rdApi/searchSchool.ts
+++ src/lib/api/3rdApi/searchSchool.ts
@@ -1,16 +1,18 @@
-import { SchoolInfo } from "./../types.d";
+import { SchoolsInfo } from "./../types.d";
 
 export const searchSchool = async (
   searchSchuNm?: string,
-): Promise<SchoolInfo[]> => {
+): Promise<SchoolsInfo> => {
   const apiKey = process.env.CAREERNET_API_KEY;
   const thisPage = 1;
   const perPage = 20;
-  const axios = require("axios").default;
+  const axios = require("axios");
+  console.log(apiKey);
+  console.log(searchSchuNm);
 
-  const { dataSearch } = await axios.get(
-    `www.career.go.kr/cnet/openapi/getOpenApi?apiKey=${apiKey}&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${thisPage}&perPage=${perPage}&searchSchulNm=${searchSchuNm}`,
+  const res = await axios.get(
+    `https://www.career.go.kr/cnet/openapi/getOpenApi.json?apiKey=a959ed4f1f173b6a1756c8943ab55e94&svcType=api&svcCode=SCHOOL&contentType=json&gubun=univ_list&thisPage=${thisPage}&perPage=${perPage}&searchSchulNm=${searchSchuNm}`,
   );
 
-  return dataSearch.content;
+  return res.data.dataSearch;
 };
