683e7f2201a27496ebfdccbe649a560935d0340b
dja1716 <hdum@uwaterloo.ca>
2022-11-16 12:24:56 +0900

    new: Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import { PureComponent } from "react";
+
+interface Props {}
+
+export default class Footer extends PureComponent<Props> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div></div>;
+  }
+}
