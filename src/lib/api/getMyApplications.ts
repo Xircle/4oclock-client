b9b6780a7ddd8f69c800206f3aee6a393c43f3a2
dja1716 <hdum@uwaterloo.ca>
2022-11-16 14:17:13 +0900

    edit: getMyApplications

--- src/lib/api/getMyApplications.ts
+++ src/lib/api/getMyApplications.ts
@@ -1,6 +1,5 @@
-import { checkResponseStatus } from "./../checkResponseStatus";
 import AxiosClient from "../apiClient";
-import { GetMyApplicationsOutput, MyApplicationsByStatus } from "./types";
+import { GetMyApplicationsOutput } from "./types";
 
 export const getMyApplications = async (): Promise<GetMyApplicationsOutput> => {
   const response = await AxiosClient.get<GetMyApplicationsOutput>(
