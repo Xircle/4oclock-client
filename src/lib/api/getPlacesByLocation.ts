f6034a292ecc3785b49ac5ff8705a7e89e31f384
dja1716 <hdum@uwaterloo.ca>
2022-11-29 15:18:52 +0900

    edit: getPlacesByLocation

--- src/lib/api/getPlacesByLocation.ts
+++ src/lib/api/getPlacesByLocation.ts
@@ -4,15 +4,19 @@ import storage from "../storage";
 import { CURRENT_USER } from "../../components/shared/constants";
 
 export type PlaceLocation = "전체" | "안암" | "신촌";
+export type PlaceType = "Regular-meeting" | "Lightning" | "Event" | "All";
 
 export const getPlacesByLocation = async (
   selectedLocation: PlaceLocation,
   page: number = 1,
   limit: number = 10,
+  selectedPlaceType?: PlaceType,
 ): Promise<GetPlacesByLocationOutput | undefined> => {
   if (!storage.getItem(CURRENT_USER)) return;
   const { data } = await AxiosClient.get<GetPlacesByLocationOutput>(
-    `/place?location=${selectedLocation}&page=${page}&limit=${limit}`,
+    selectedPlaceType
+      ? `/place?location=${selectedLocation}&page=${page}&limit=${limit}&placeType=${selectedPlaceType}`
+      : `/place?location=${selectedLocation}&page=${page}&limit=${limit}`,
     {
       headers: {
         Authorization: `Bearer ${storage.getItem(CURRENT_USER)["token"]}`,
