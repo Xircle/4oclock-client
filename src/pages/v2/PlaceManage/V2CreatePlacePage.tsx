30ea001a3eb1968395f89864ebca288e90d3168c
dja1716 <hdum@uwaterloo.ca>
2022-12-01 18:13:06 +0900

    edit: V2CreatePlacePage

--- src/pages/v2/PlaceManage/V2CreatePlacePage.tsx
+++ src/pages/v2/PlaceManage/V2CreatePlacePage.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { RootState } from "../../../lib/v2/reducers/reducer";
 
 export default function V2CreatePlacePage() {
-  return <div></div>;
+  const {} = useSelector((state: RootState) => state.placeReducer);
+  return <Container></Container>;
 }
+
+const Container = styled.div``;
