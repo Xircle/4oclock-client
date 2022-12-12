40fad18b00810241d1f5c78241f31d89b6a290bb
dja1716 <hdum@uwaterloo.ca>
2022-12-12 12:18:58 +0900

    new: V2CreateTeamPage

---
+++ src/pages/v2/Team/V2CreateTeamPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+interface Props {
+  leaderId: string;
+}
+
+export default function V2CreateTeamPage({ leaderId }: Props) {
+  return <Container>create team page</Container>;
+}
+
+const Container = styled.div`
+  width: 100%;
+`;
