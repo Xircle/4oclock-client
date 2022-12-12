2f066a90bce4a85a0e31b84b59c1c83b8decc213
dja1716 <hdum@uwaterloo.ca>
2022-12-12 12:51:44 +0900

    edit: TeamReducer

--- src/pages/v2/Team/V2CreateTeamPage.tsx
+++ src/pages/v2/Team/V2CreateTeamPage.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { teamInitialState, teamReducer } from "./TeamReducer";
 
 interface Props {
   leaderId: string;
 }
 
 export default function V2CreateTeamPage({ leaderId }: Props) {
+  const history = useHistory();
+  const [step, setStep] = useState(0);
+  const [state, dispatch] = useReducer(teamReducer, teamInitialState);
+  const [isLoading, setIsLoading] = useState(false);
+
   return <Container>create team page</Container>;
 }
 
