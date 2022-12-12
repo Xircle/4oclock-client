5a65b5029bbc933b41213c5da24e2f97ff7f81f9
dja1716 <hdum@uwaterloo.ca>
2022-12-12 12:33:20 +0900

    new: TeamReducer

---
+++ src/pages/v2/Team/TeamReducer.ts
@@ -0,0 +1,14 @@
+export const teamInitialState = {
+  leaderId: "",
+  images: [],
+  season: 6,
+  max_participant: 20,
+  categoryId: "",
+  question: "",
+  leaderIntro: "",
+  meeting_day: 0,
+  meeting_hour: 0,
+  areaIds: "",
+  activityTitles: [],
+  activityDetails: [],
+};
