9026c7b16988f48e8419815794cc5be7329904f6
dja1716 <hdum@uwaterloo.ca>
2022-12-12 19:38:41 +0900

    edit: CT2

--- src/components/team/CreateTeam2.tsx
+++ src/components/team/CreateTeam2.tsx
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import { colors, MidInput } from "../../styles/styles";
+import Label from "./Label";
 import { TeamAction, TeamState } from "./types";
 
 interface Props {
@@ -7,5 +10,22 @@ interface Props {
 }
 
 export default function CreateTeam2({ onNext, state, dispatch }: Props) {
-  return <div></div>;
+  return (
+    <Container>
+      <Label mandatory={true} labelName="클럽 활동 소개" />
+      <Label
+        mandatory={true}
+        labelName="클럽소개 세부글"
+        description="진행하실 클럽에 대한 소개를 적어주세요!"
+      />
+      <Label
+        mandatory={true}
+        labelName="클럽 소개 사진 올리기(2개 필수)"
+        description="설명만으로는 부족한 my클럽을 표현하는 사진을 올려주세요!
+사진을 통해 클럽의 매력을 발산해주세요!"
+      />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
