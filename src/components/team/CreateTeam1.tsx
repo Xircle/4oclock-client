735c1e060185f9036f52f25f45724fd45bcaf21d
dja1716 <hdum@uwaterloo.ca>
2022-12-12 19:14:38 +0900

    edit: CreateTeam1

--- src/components/team/CreateTeam1.tsx
+++ src/components/team/CreateTeam1.tsx
@@ -1,5 +1,7 @@
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
-import { BigTextArea, MidInput } from "../../styles/styles";
+import { BigTextArea, colors, MainBtn, MidInput } from "../../styles/styles";
 import Label from "./Label";
 import { TeamAction, TeamState } from "./types";
 
@@ -30,9 +32,25 @@ export default function CreateTeam1({ onNext, state, dispatch }: Props) {
       />
       <CancelWordCount>{state.name?.length}/30</CancelWordCount>
       <Label mandatory={true} labelName="만나는 정기모임 요일 및 시간" />
+      <DropDownButton>
+        <DropDownText>매주 만나는 요일 및 시간을 골라주세요</DropDownText>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </DropDownButton>
       <Label mandatory={true} labelName="주 활동지역 (복수 선택 가능)" />
+      <DropDownButton>
+        <DropDownText>매주 만나는 장소를 골라주세요</DropDownText>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </DropDownButton>
       <Label mandatory={true} labelName="클럽 나이대" />
+      <DropDownButton>
+        <DropDownText>해당 클럽의 활동 테마를 골라주세요</DropDownText>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </DropDownButton>
       <Label mandatory={true} labelName="클럽 활동테마" />
+      <DropDownButton>
+        <DropDownText>해당 클럽의 활동테마를 골라주세요</DropDownText>
+        <FontAwesomeIcon icon={faChevronDown} />
+      </DropDownButton>
       <Label mandatory={true} labelName="리더 자기소개" />
       <MultilineInput
         name="leaderIntro"
@@ -53,9 +71,33 @@ export default function CreateTeam1({ onNext, state, dispatch }: Props) {
         //onKeyUp={() => Validate()}
       />
       <CancelWordCount>{state.leaderIntro?.length}/30</CancelWordCount>
+      <BlankSpace />
+      <NextButton type="submit" disabled={false} onClick={onNext}>
+        다음(1/3)
+      </NextButton>
     </Container>
   );
 }
+
+export const NextButton = styled(MainBtn)`
+  position: fixed;
+  z-index: 3;
+  bottom: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #21e19c;
+  font-weight: 700;
+  font-size: 16px;
+  color: #ffffff;
+  &:disabled {
+    background-color: ${colors.LightGray};
+    filter: none;
+  }
+`;
+
+const BlankSpace = styled.div`
+  height: 100px;
+`;
 
 const CancelWordCount = styled.div`
   color: #c4cbd8;
@@ -72,4 +114,27 @@ const OnelineInput = styled(MidInput)`
   width: 100%;
 `;
 
-const MultilineInput = styled(BigTextArea)``;
+const MultilineInput = styled(BigTextArea)`
+  width: 100%;
+`;
+
+const DropDownButton = styled.div`
+  width: 100%;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #c4cbd8;
+  padding: 10px 8px;
+  margin-bottom: 28px;
+  border-radius: 8px;
+  font-size: 14px;
+
+  color: #c4cbd8;
+`;
+
+const DropDownText = styled.div`
+  font-size: 14px;
+
+  color: #c4cbd8;
+`;
