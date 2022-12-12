14b7e49f96a531ba329e758f2941a2a3f5a4ee41
dja1716 <hdum@uwaterloo.ca>
2022-12-12 19:41:01 +0900

    edit: CT3

--- src/components/team/CreateTeam3.tsx
+++ src/components/team/CreateTeam3.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { BlankSpace, NextButton } from "./CreateTeam1";
 import Label from "./Label";
 import { TeamAction, TeamState } from "./types";
 
@@ -13,6 +14,10 @@ export default function CreateTeam3({ onNext, state, dispatch }: Props) {
     <Container>
       <Label mandatory={true} labelName="클럽 활동 회차 정보" />
       <Label mandatory={true} labelName="(선택)클럽 활동 mission rule 소개" />
+      <BlankSpace />
+      <NextButton type="submit" disabled={false} onClick={onNext}>
+        다음(3/3)
+      </NextButton>
     </Container>
   );
 }
