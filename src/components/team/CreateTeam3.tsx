acef7295c259df0dd0045b1a260d495ffbbe8909
dja1716 <hdum@uwaterloo.ca>
2022-12-12 19:38:51 +0900

    edit: CT3

--- src/components/team/CreateTeam3.tsx
+++ src/components/team/CreateTeam3.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+import Label from "./Label";
 import { TeamAction, TeamState } from "./types";
 
 interface Props {
@@ -7,5 +9,12 @@ interface Props {
 }
 
 export default function CreateTeam3({ onNext, state, dispatch }: Props) {
-  return <div></div>;
+  return (
+    <Container>
+      <Label mandatory={true} labelName="클럽 활동 회차 정보" />
+      <Label mandatory={true} labelName="(선택)클럽 활동 mission rule 소개" />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
